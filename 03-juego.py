6a60103c7fdd3eac293f66042925110b554f738e
kaleth.dev <dev.kaleth.mendoza@gmail.com>
2026-03-31 21:27:34 -0500

    [ fit ] - error terminate game

--- 03-juego.py
+++ 03-juego.py
@@ -11,9 +11,11 @@ while intentos > 0:
     respuesta = input("Vuelo sin alas, lloro sin ojos. ¿Qué soy? ").lower()
     if respuesta == respuesta_correcta:
         print("🎉 ¡Correcto! Ganaste.")
-        if intentos == 3 :
+        if respuesta == respuesta_correcta :
             pregunta = input("Me puedes romper sin tocarme, puedes decirme sin hablar. ¿Qué soy?")
-            print("🎉 ¡Correcto! Ganaste.")
+            if pregunta == respuesta_correcta2 :
+                print("🎉 ¡Correcto! Ganaste.")
+                break
         else : 
             intentos -= 1
             print("❌ Incorrecto. Te quedan", intentos, "intentos.\n")
