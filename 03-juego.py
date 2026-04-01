ee74b3d6f1344d3cb6e015a4fb049b256df3fbb9
kaleth.dev <dev.kaleth.mendoza@gmail.com>
2026-03-31 21:18:32 -0500

    [ feat ] - new question

--- 03-juego.py
+++ 03-juego.py
@@ -4,17 +4,23 @@ print("🧠 Bienvenido al juego de acertijos")
 print("Adivina la respuesta correcta para ganar\n")
 
 respuesta_correcta = "sombra"
+respuesta_correcta2 = "promesa"
 intentos = 3
 
 while intentos > 0:
     respuesta = input("Vuelo sin alas, lloro sin ojos. ¿Qué soy? ").lower()
-
     if respuesta == respuesta_correcta:
         print("🎉 ¡Correcto! Ganaste.")
-        break
+        if intentos == 3 :
+            pregunta = input("Me puedes romper sin tocarme, puedes decirme sin hablar. ¿Qué soy?")
+            print("🎉 ¡Correcto! Ganaste.")
+        else : 
+            intentos -= 1
+            print("❌ Incorrecto. Te quedan", intentos, "intentos.\n")
     else:
         intentos -= 1
         print("❌ Incorrecto. Te quedan", intentos, "intentos.\n")
+    
 
 if intentos == 0:
     print("💀 Perdiste. La respuesta era:", respuesta_correcta)
